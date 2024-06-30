43f402d190e24997bbf623f005697f25a32d56f8
S-Dionis <denis.olegovich90@gmail.com>
2024-06-30 22:06:17 +0300

    hw07_file_copying

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,127 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
+	ErrFileNotExists         = errors.New("file is not exists")
+	ErrWrongOffsetValue      = errors.New("parameter offset is not correct")
+	ErrWrongLimitValue       = errors.New("parameter limit is not correct")
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+var closeFile = func(file *os.File) {
+	if err := file.Close(); err != nil {
+		panic(err)
+	}
+}
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if offset < 0 {
+		return ErrWrongOffsetValue
+	}
+
+	if limit < 0 {
+		return ErrWrongLimitValue
+	}
+
+	file, err := os.Open(fromPath)
+	defer closeFile(file)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			return ErrFileNotExists
+		}
+
+		return ErrUnsupportedFile
+	}
+
+	fileStat, err := file.Stat()
+	panicOnErr(err)
+
+	if fileStat.Size() < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit == 0 {
+		limit = fileStat.Size()
+	}
+
+	to, err := os.Create(toPath)
+	panicOnErr(err)
+
+	defer closeFile(to)
+
+	err = copyFromTo(file, to, offset, limit)
+	panicOnErr(err)
+
 	return nil
+}
+
+func copyFromTo(from *os.File, to *os.File, offset, limit int64) error {
+	_, err := from.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+
+	var bufSize int64 = 1024
+
+	if limit < bufSize {
+		bufSize = limit
+	}
+
+	buff := make([]byte, bufSize)
+	count := limit / bufSize
+	rest := limit % bufSize
+
+	total := int(count)
+	if rest > 0 {
+		total++
+	}
+
+	bar := pb.StartNew(total)
+
+	for i := 0; i < int(count); i++ {
+		n, err := from.Read(buff)
+		if err != nil {
+			return err
+		}
+
+		if n == 0 || err == io.EOF {
+			break
+		}
+
+		writeOrPanic(to, buff[:n])
+		bar.Increment()
+	}
+
+	if rest > 0 {
+		buff = make([]byte, rest)
+		n, err := from.Read(buff)
+		if err != nil {
+			return err
+		}
+		writeOrPanic(to, buff[:n])
+		bar.Increment()
+	}
+
+	bar.Finish()
+
+	return nil
+}
+
+func writeOrPanic(to *os.File, buff []byte) {
+	_, err := to.Write(buff)
+	panicOnErr(err)
+}
+
+func panicOnErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,75 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"log"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func ActionWithLogError(fn func() error) {
+	err := fn()
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func logErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	path := "testdata/testFile.txt"
+
+	t.Run("Tasks without errors", func(t *testing.T) {
+		err := Copy("testdata/input.txt", path, 0, 0)
+		require.NoError(t, err)
+		inputFile, err := os.Open("testdata/input.txt")
+		require.NoError(t, err)
+		logErr(err)
+
+		testFile, _ := os.Open(path)
+		inputFileStat, _ := inputFile.Stat()
+		testFileStat, _ := testFile.Stat()
+		expectedSize := inputFileStat.Size()
+		actualSize := testFileStat.Size()
+
+		ActionWithLogError(func() error { return inputFile.Close() })
+		ActionWithLogError(func() error { return testFile.Close() })
+		ActionWithLogError(func() error { return os.Remove(path) })
+
+		require.Truef(t, expectedSize == actualSize, "Equality of file size")
+	})
+
+	t.Run("Errors on copying", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "./testFile", -1, 10000)
+		require.Truef(t, errors.Is(err, ErrWrongOffsetValue), "actual err - %v", err)
+		err = Copy("testdata/input.txt", "./testFile", 0, -1)
+		require.Truef(t, errors.Is(err, ErrWrongLimitValue), "actual err - %v", err)
+	})
+
+	t.Run("Offset and limit test", func(t *testing.T) {
+		err := Copy("testdata/input.txt", path, 1000, 100)
+		require.NoError(t, err)
+
+		// dd if=input.txt of=output_1000_100.txt bs=1 skip=1000 count=100
+		expected, _ := os.ReadFile("testdata/output_1000_100.txt")
+		file, _ := os.ReadFile("testdata/testFile.txt")
+		content := string(file)
+		expectedContent := string(expected)
+
+		compare := strings.Compare(content, expectedContent)
+		require.Truef(t, compare == 0, "File content as expected")
+
+		err = os.Remove(path)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	})
 }
