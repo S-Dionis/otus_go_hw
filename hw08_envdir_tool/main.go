40ec39233a827dee36cb867d79c4c44eeff0efef
S-Dionis <denis.olegovich90@gmail.com>
2024-07-07 23:49:16 +0300

    hw08_envdir_tool remove redurant cmd call

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"os/exec"
 )
 
 func main() {
-	command := exec.Command("/bin/bash", "a")
-	command.Stdout = os.Stdout
-	command.Stderr = os.Stderr
-	command.Stdin = os.Stdin
-	err := command.Run()
-
-	fmt.Println(err)
-
 	if len(os.Args) < 3 {
 		log.Fatalf("Usage: %s <envdir> <command> [args...]", os.Args[0])
 	}
